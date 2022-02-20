cc13bca2635eee5b0d7205ab03b35fa601823800
santibv15 <sbeleno809@misena.edu.co>
2022-02-20 13:14:01 -0500

    agregar cambios a interfaz externa y menu interno de formador

---
+++ views/interfaz_interna/formador/clase.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/interfaz_interna/formador/clase.css">
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body id="body">
+    <header>
+        <div class="icon__menu"><i class='bx bx-menu' id="btn_open"></i></div>
+    </header>
+
+    <div class="menu__side" id="menu__side">
+
+        <div class="name__page">
+            <img src="../../img/logos/logotipo3.png" id="icono-kinesfera" alt="">
+            <h4 id="titulo-kinesfera">Kinesfera<span style="color: transparent;">_</span>Lab</h4>
+        </div>
+
+        <div class="options__menu">
+
+            <a href="" class="selected">
+                <div class="option">
+                    <i class='bx bxs-home' title="inicio"></i>
+                    <h4>Clases</h4>
+                </div>
+            </a>
+            <a href="">
+                <div class="option">
+                    <i class='bx bx-world' title="Explorar"></i>
+                    <h4>Explorar</h4>
+                </div>
+            </a>
+            <a href="">
+                <div class="option">
+                    <i class='bx bx-edit' title="registrar aprendiz"></i>
+                    <h4>Registrar aprendiz</h4>
+                </div>
+            </a>
+            <a href="">
+                <div class="option">
+                    <i class='bx bxs-folder-open' title="Laboratorio Artistico"></i>
+                    <h4>Laboratorio Artistico</h4>
+                </div>
+            </a>
+            <a href="">
+                <div class="option">
+                    <i class='bx bx-user' title="perfil"></i>
+                    <h4>Perfil</h4>
+                </div>
+            </a>
+            <a href="">
+                <div class="option">
+                    <i class='bx bx-log-in' title="salir"></i>
+                    <h4>Salir</h4>
+                </div>
+            </a>
+        </div>
+
+    </div>
+
+    <main>
+        
+    </main>
+    <script src="../../js/interfaz_interna/menu.js"></script>
+</body>
+</html>
+
---
+++ views/js/interfaz_interna/menu.js
@@ -0,0 +1,35 @@
+//ejecutar funcion en el evento click
+document.getElementById("btn_open").addEventListener("click", open_close_menu)
+
+//declaramos variables
+var side_menu = document.getElementById("menu__side");
+var btn_open = document.getElementById("btn_open");
+var body = document.getElementById("body");
+
+//evento para mostrar y ocultar menu
+
+    function open_close_menu(){
+        body.classList.toggle("body_move");
+        side_menu.classList.toggle("menu__side_move");
+    }
+
+    //si el ancho de la pagina es menor a 760px, ocultara el meni al recargar la pagina
+
+    if (window.innerWidth < 760){
+        body.classList.add("body_move");
+        side_menu.classList.add("menu__side_move")
+    }
+
+    //haciendo el menu responsive
+
+    window.addEventListener("resize", function(){
+        if (window.innerWidth > 760){
+            body.classList.remove("body_move");
+            side_menu.classList.remove("menu__side_move");
+        }
+
+        if (window.innerWidth < 760){
+            body.classList.add("body_move");
+            side_menu.classList.add("menu__side_move");
+        }
+    })
