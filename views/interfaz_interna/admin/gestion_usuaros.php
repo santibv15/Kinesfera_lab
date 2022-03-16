c6c1b609e27366a61b45c214618911394898d176
santibv15 <sbeleno809@misena.edu.co>
2022-03-16 11:43:27 -0500

    16 de febrero interfaz gestion usuario

--- views/interfaz_interna/admin/gestion_usuaros.php
+++ views/interfaz_interna/admin/gestion_usuaros.php
@@ -7,6 +7,38 @@
     <title>Document</title>
 </head>
 <body>
-    <tr>
+    <h1>Gestion de usuarios</h1>
+    <div class="container_buscador"><input type="search" placeholder="Buscar usuario"></div>
+    <table class="tabla_usuarios">
+
+        <tr>
+        <th>Usuarios Kinesfera</th>
+        <th>Informacion</th>
+        <th>Formador</th>
+        <th>Aprendiz</th>
+        <th>Eliminar</th>
+        </tr>
+        <tr>
+        <td>Pedro Jose</td>
+        <td><input type="button" value="Más"></td>
+        <td><input type="button" value="F"></td>
+        <td><input type="button" value="A"></td>
+        <td><input type="button" value="X"></td>
+        </tr>
+        <tr>
+        <td>Maicol Raul</td>
+        <td><input type="button" value="Más"></td>
+        <td><input type="button" value="F"></td>
+        <td><input type="button" value="A"></td>
+        <td><input type="button" value="X"></td>
+        </tr>
+        <tr>
+        <td>Juan Moral</td>
+        <td><input type="button" value="Más"></td>
+        <td><input type="button" value="F"></td>
+        <td><input type="button" value="A"></td>
+        <td><input type="button" value="X"></td>
+        </tr>
+    </table>
 </body>
 </html>
