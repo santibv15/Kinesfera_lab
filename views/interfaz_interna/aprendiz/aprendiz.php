10d2972ed7999ba0d0f42cb7f14ab4222008fb2c
santibv15 <sbeleno809@misena.edu.co>
2022-03-28 00:51:47 -0500

    28 de febrero actualizacion de aprendiz

--- views/interfaz_interna/aprendiz/aprendiz.php
+++ views/interfaz_interna/aprendiz/aprendiz.php
@@ -20,10 +20,65 @@ if (!isset($_SESSION['ID_USUARIO'])){
     <title>Document</title>
     <link rel="stylesheet" href="../../css/interfaz_interna/aprediz/aprendiz.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
 </head>
-<body>
+<body id="body">
     <!--<h1>Aprendizz <?php echo $_SESSION['NOMBRES_USUARIO'];?></h1>-->
+    
+<header>
+    <div class="icon__menu"><i class='bx bx-menu' id="btn_open"></i></div>
+</header>
+
+<div class="menu__side" id="menu__side">
+
+    <div class="name__page">
+        <img src="../../img/logos/logotipo3.png" id="icono-kinesfera" alt="">
+        <h4 id="titulo-kinesfera">Kinesfera<span style="color: transparent;">_</span>Lab</h4>
+    </div>
+
+    <div class="options__menu">
+
+        <a href="" >
+            <div class="option">
+                <i class='bx bxs-home' title="inicio"></i>
+                <h4>Clases</h4>
+            </div>
+        </a>
+        <a href="">
+            <div class="option">
+                <i class='bx bx-world' title="Explorar"></i>
+                <h4>Explorar</h4>
+            </div>
+        </a>
+        <a href="" >
+            <div class="option" >
+                <i class='bx bxs-folder-open'  title="Laboratorio Artistico"></i>
+                <h4>Laboratorio Artistico</h4>
+            </div>
+        </a>
+        
+        <a href="" class="selected">
+            <div class="option">
+                <i class='bx bx-user' title="perfil"></i>
+                <h4>Perfil</h4>
+            </div>
+        </a>
+        <a href="../../../controllers/login/salir.php" onclick=" return confirmSalir()">
+            <div class="option option_uni">
+                <i class='bx bx-log-in' title="salir"></i>
+                <h4>Salir</h4>
+            </div>
+        </a>
+    </div>
+
+</div>
+<main>
+    
+</main>
+
+
+<!--FIN DE MENU DEL ADMINISTRADOR-->
+
 
     <section class="seccion-perfil-usuario">
         <div class="perfil-usuario-header">
@@ -65,5 +120,7 @@ if (!isset($_SESSION['ID_USUARIO'])){
         </div>
     </section>
     
+    <script src="../../js/interfaz_interna/menu.js"></script>
+    <script src="../../js/interfaz_interna/alertas.js"></script>   
 </body>
 </html>
